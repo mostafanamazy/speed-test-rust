c9c5fd56f330396660da61f8a970b2677bfca5e2
Mostafa Namazi <mostafa.namazy@gmail.com>
2020-06-12 09:51:36 +0430

    test invalid second in clinet's config file.

--- tests/config.rs
+++ tests/config.rs
@@ -3,15 +3,21 @@ use speed_test_rust::config;
 
 #[test]
 fn test_read_client() {
-    let conf = config::read_client(Some("config/testfile/1c.ini"));
+    let conf = config::read_client(Some("config/testfiles/1c.ini"));
     assert_eq!(conf.address, "127.0.0.1:55");
     assert_eq!(conf.timeout, 10);
     assert_eq!(conf.down_time, 30);
     assert_eq!(conf.up_time, 30);
+
+    let conf = config::read_client(Some("config/testfiles/2c.ini"));
+    assert_eq!(conf.address, "192.168.0.1:80");
+    assert_eq!(conf.timeout, 10);
+    assert_eq!(conf.down_time, 10);
+    assert_eq!(conf.up_time, 10);
 }
 
 #[test]
 fn test_read_server() {
-    let conf = config::read_server(Some("config/testfile/1s.ini"));
+    let conf = config::read_server(Some("config/testfiles/1s.ini"));
     assert_eq!(conf, "127.0.0.1:800");
 }
